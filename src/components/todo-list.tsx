e4eea9c357a59e85e2df6b46b798cb526d5d2dc6
kazuma-fujita <kazuma.fujita.zuma@gmail.com>
2023-01-10 20:06:51 +0900

    fetch todo items

--- src/components/todo-list.tsx
+++ src/components/todo-list.tsx
@@ -1,10 +1,46 @@
+import { gql, request } from "graphql-request";
+import useSWR from "swr";
+
+type TodoResult = {
+  todos: Todos;
+};
+
+type Todos = {
+  data: Todo[];
+};
+
+type Todo = {
+  id: string;
+  title: string;
+  completed: boolean;
+};
+
+const query = gql`
+  query {
+    todos {
+      data {
+        id
+        title
+        completed
+      }
+    }
+  }
+`;
+
+const fetcher = (query: string) =>
+  request("https://graphqlzero.almansi.me/api", query);
+
 export const TodoList = () => {
+  const { data, error } = useSWR<TodoResult>(query, fetcher);
+  console.log("data", data);
   return (
     <ul>
-      <li>item</li>
-      <li>item</li>
-      <li>item</li>
-      <li>item</li>
+      {data &&
+        data.todos.data.map((todo) => (
+          <li key={todo.id}>
+            No.{todo.id} {todo.title}
+          </li>
+        ))}
     </ul>
   );
 };
