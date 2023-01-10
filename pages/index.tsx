0ba360dcea960742e8faa38ef5ea448ab1d7dc4a
kazuma-fujita <kazuma.fujita.zuma@gmail.com>
2023-01-10 19:36:56 +0900

    added graphql requests

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,10 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from '@next/font/google'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
+import Image from "next/image";
+import { Inter } from "@next/font/google";
+import styles from "../styles/Home.module.css";
+import { TodoList } from "../src/components/todo-list";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
@@ -26,7 +27,7 @@ export default function Home() {
               target="_blank"
               rel="noopener noreferrer"
             >
-              By{' '}
+              By{" "}
               <Image
                 src="/vercel.svg"
                 alt="Vercel Logo"
@@ -37,6 +38,10 @@ export default function Home() {
               />
             </a>
           </div>
+        </div>
+
+        <div className={styles.center}>
+          <TodoList />
         </div>
 
         <div className={styles.center}>
@@ -119,5 +124,5 @@ export default function Home() {
         </div>
       </main>
     </>
-  )
+  );
 }
---
+++ src/components/todo-list.tsx
@@ -0,0 +1,10 @@
+export const TodoList = () => {
+  return (
+    <ul>
+      <li>item</li>
+      <li>item</li>
+      <li>item</li>
+      <li>item</li>
+    </ul>
+  );
+};
